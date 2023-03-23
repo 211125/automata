a9203cb755b9d32be78366b1cfe4a18c8d88c57b
Osvaldo Angel <211125@ids.upchiapas.edu.mx>
2023-03-23 06:27:36 -0600

    1.2

--- software.py
+++ software.py
@@ -1,6 +1,9 @@
 # Pedir al usuario que ingrese el ciclo for
 for_loop = input("Ingrese el ciclo for: ")
 
+# Eliminar espacios innecesarios
+for_loop = " ".join(for_loop.split())
+
 # Analizar la sintaxis del ciclo for para extraer las partes necesarias
 parts = for_loop.split("(")[1].split(")")[0].split(";")
 
