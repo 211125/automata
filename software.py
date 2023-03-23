bda3dcd76992507fc48d147b88bf86359a2c5b3a
Osvaldo Angel <211125@ids.upchiapas.edu.mx>
2023-03-23 06:19:01 -0600

    automata

--- software.py
+++ software.py
@@ -1,43 +1,12 @@
-# Pedir al usuario que ingrese el código del bucle for
-for_code = input("Ingrese el código del bucle for: ")
+# Pedir al usuario que ingrese el ciclo for
+for_loop = input("Ingrese el ciclo for: ")
 
-# Eliminar los caracteres no deseados del código del bucle for
-for_code = for_code.replace("(", "").replace(")", "").replace("{", "").replace("}", "").strip()
+# Analizar la sintaxis del ciclo for para extraer las partes necesarias
+parts = for_loop.split("(")[1].split(")")[0].split(";")
 
-# Extraer la variable y el rango del bucle for
-var, range_str = "", ""
-if "int " in for_code:
-    _, var, range_str = for_code.split(" ")
-    var = var.strip()
-else:
-    var, range_str = for_code.split(";")[0], for_code.split(";")[1]
-var = var.strip()
+# Crear la expresión while a partir de las partes extraídas
+while_loop = "while (" + parts[0] + ") {\n" + parts[1] + ";\n" + parts[2] + "\n}"
 
-# Extraer los límites del rango del bucle for
-range_start, range_end, range_inc = "", "", ""
-for s in range_str.split(";"):
-    s = s.strip()
-    if s.startswith(var):
-        range_start = s.split("=")[1].strip()
-    elif s.startswith("<"):
-        range_end = s.split("<")[1].strip()
-    elif s.startswith(range_end + "+"):
-        range_inc = s.split("+")[1].strip()
-if range_start == "" or range_end == "":
-    print("Error: no se pudo encontrar el rango del bucle for")
-    exit()
-
-# Convertir los límites del rango a números enteros
-range_start = int(eval(range_start))
-range_end = int(eval(range_end))
-if range_inc == "":
-    range_inc = 1
-else:
-    range_inc = int(eval(range_inc))
-
-# Generar el código equivalente del bucle while
-while_code = "{0} = {1}\nwhile {0} < {2}:\n    {3}\n    {0} += {4}".format(var, range_start, range_end, for_code.split(";", 2)[2].strip(), range_inc)
-
-# Imprimir el código equivalente del bucle while
-print("El equivalente del bu")
-print(while_code)
+# Mostrar el ciclo while resultante al usuario
+print("El ciclo while equivalente es:\n")
+print(while_loop)
